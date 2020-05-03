99f137d208a91f00824fea3ef51a386f1ba84327
Mohamed Anis MANI <manimohamed@gmail.com>
2020-05-03 18:53:19 +0100

    Finished writing exams endpoints tests. Now writing enrolls endpoints tests.

--- manage.py
+++ manage.py
@@ -1,9 +1,18 @@
+import os
+import sys
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import db
-from runapp import app
+from app import db, create_app
 
+RUN_CONFIG = os.getenv('FLASK_ENV', 'default')
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'tests':
+        RUN_CONFIG = 'testing'
+os.environ['config'] = RUN_CONFIG
+
+app = create_app(RUN_CONFIG)
 migrate = Migrate(app, db)
 manager = Manager(app)
 
